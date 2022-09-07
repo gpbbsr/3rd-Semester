fd48c512cf24d4c40c5c05bf7a4cd1c21f91444d
Abinash Patra <patraabinash1028@gmail.com>
2022-09-07 12:25:33 +0530

    experiments added

---
+++ OOPS Using Java/Experiments/exp-01/hello.java
@@ -0,0 +1,9 @@
+//Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+
+public class hello
+{
+public static void main(String sa[])
+{
+System.out.println("Hello World");
+}
+}
